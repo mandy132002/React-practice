0519d0f9b0bfd1c19187ed72ca21bd334910f1bb
Mandar Prakash Kulkarni <mandar4567@gmail.com>
2023-01-05 12:30:28 +0530

    initial commit

---
+++ src/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <>
+        <footer className="bg-black text-white fixed bottom-0">
+                <div className="grid grid-rows-1 grid-flow-col gap-8">
+                    <div className="p-6">
+                        Text dalega idhar mai bohot sara logo ko samjhega nahi kya likha hai
+                    </div>
+                    <div className="p-6">
+                        Text dalega idhar mai bohot sara logo ko samjhega nahi kya likha hai
+                    </div>
+                    <div className="p-6">
+                        Text dalega idhar mai bohot sara logo ko samjhega nahi kya likha hai
+                    </div>
+                </div>
+        </footer>
+        </>
+    )
+}
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Header(){
+    return (
+        <header>
+                <nav className="navbar mb-4 bg-black text-white flex justify-between">
+                <img className="image w-1/12" src="./images/logo1.png"  />
+                <ul className="list-items flex text-2xl space-x-6 mr-6" >
+                    <li className='p-4'>Events</li>
+                    <li className='p-4'>About</li>
+                    <li className='p-4'>Contact us</li>
+                </ul>
+                </nav>
+        </header>
+    )
+}
+
+export default Header;
